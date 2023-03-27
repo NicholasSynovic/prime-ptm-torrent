d8eb0373b27baae9ebfeeeceba3fef3e125adeac
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-27 20:08:13 +0000

    Add code to get the commits information

---
+++ prime_ptm_torrent/commitsPipeline.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fullPath=$1
+author=${fullPath##*/}	# https://stackoverflow.com/a/1371283
+
+ptmDirs=$(ls $fullPath)
+
+ls $fullPath | parallel -j 10 -k --bar clime-git-commits-extract -d ${fullPath}/{} -o ../../data/commits/${author}_{}_commits_loc.json --log ../../data/commits/${author}_{}_log.log
