fb99cded76838f2d44c31f58c7b849f51d8cc742
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-28 10:46:13 -0500

    Add code to execute clime-issues functionality

--- prime_ptm_torrent/issuesPipelinePreprocess.py
+++ prime_ptm_torrent/issuesPipelinePreprocess.py
@@ -25,8 +25,8 @@ def getArgs() -> Namespace:
     return parser.parse_args()
 
 
-def runCommand(url: str, authorRepo: str, token: str) -> None:
-    cmd_str: str = f"clime-gh-issues -r {url} -o {authorRepo}_gh_issues.json -t {token} --log {authorRepo}_gh_issues_log.log"
+def runCommand(url: str, jsonFileName: str, token: str) -> None:
+    cmd_str: str = f"clime-gh-issues -r {url} -o {jsonFileName} -t {token} --log {url.replace('/', '_')}_gh_issues_log.log"
     subprocess.run(cmd_str, shell=True)
 
 
@@ -35,11 +35,11 @@ def main() -> None:
 
     line: str
     for line in open(args.file, "r"):
-        url: str = line.strip()
-        parsedURL: ParseResult = urlparse(url=line)
-        authorRepo: str = parsedURL.path.strip("/").replace("/", "_")
+        parsedURL: ParseResult = urlparse(url=line.strip())
+        url: str = parsedURL.path.strip("/")
+        jsonFileName: str = f'{url.replace("/", "_")}_gh_issues.json'
 
-        runCommand(url, authorRepo, token=args.token)
+        runCommand(url, jsonFileName, token=args.token)
 
 
 if __name__ == "__main__":
