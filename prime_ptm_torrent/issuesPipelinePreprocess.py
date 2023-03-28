f36b79bd0202bc0d70e546de5f499bf9ef836b91
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-28 10:24:39 -0500

    Add code to run clime issues

--- prime_ptm_torrent/issuesPipelinePreprocess.py
+++ prime_ptm_torrent/issuesPipelinePreprocess.py
@@ -1,3 +1,4 @@
+import subprocess
 from argparse import ArgumentParser, Namespace
 from typing import List
 from urllib.parse import ParseResult, urlparse
@@ -12,11 +13,21 @@ def getArgs() -> Namespace:
     parser.add_argument(
         "-f",
         "--file",
-        default="ghRepos.txt",
         required=True,
         help="A file containing GitHub repo remote origin links to get issue data from",
     )
+    parser.add_argument(
+        "-t",
+        "--token",
+        required=True,
+        help="A GitHub personal access token",
+    )
     return parser.parse_args()
+
+
+def runCommand(url: str, authorRepo: str, token: str) -> None:
+    cmd_str: str = f"clime-gh-issues -r {url} -o {authorRepo}_gh_issues.json -t {token} --log {authorRepo}_gh_issues_log.log"
+    subprocess.run(cmd_str, shell=True)
 
 
 def main() -> None:
@@ -27,7 +38,6 @@ def main() -> None:
         line = line.strip()
         url: ParseResult = urlparse(url=line)
         authorRepo: str = url.path.strip("/").replace("/", "_")
-        print(authorRepo)
 
 
 if __name__ == "__main__":
