30739f45d5702b0085f8ac4a81d0b0eb41d42f9e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-28 10:14:52 -0500

    Add code for the issues pipeline

--- prime_ptm_torrent/issuesPipeline.py
+++ prime_ptm_torrent/issuesPipeline.py
@@ -1,12 +1,34 @@
 from argparse import ArgumentParser, Namespace
+from typing import List
+from urllib.parse import ParseResult, urlparse
 
-def getArgs()   ->  Namespace:
-    parser: ArgumentParser = ArgumentParser()
-    parser.add_arg("-u", "--url", required=True)
+
+def getArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog="PRIME PTMTorrent Issues Pipeline",
+        usage="The script to handle collecting issue data from PTMTorrent repositories",
+        epilog="Written by Nicholas M. Synovic",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        default="ghRepos.txt",
+        required=True,
+        help="A file containing GitHub repo remote origin links to get issue data from",
+    )
     return parser.parse_args()
 
-def main()  ->  None:
-    pass
+
+def main() -> None:
+    args: Namespace = getArgs()
+
+    line: str
+    for line in open(args.file, "r"):
+        line = line.strip()
+        url: ParseResult = urlparse(url=line)
+        authorRepo: str = url.path.strip("/").replace("/", "_")
+        print(authorRepo)
+
 
 if __name__ == "__main__":
     main()
--- prime_ptm_torrent/setupGlobus.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-globus login --no-local-server
