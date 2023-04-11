647f24f1e84a855459eb16ebb92e70ec2095d3c7
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-11 12:41:12 -0500

    Set the output directory as a variable

--- prime_ptm_torrent/commitsPipeline.bash
+++ prime_ptm_torrent/commitsPipeline.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir ../../data/commits
-
+dataDir="~/data/commits"
 fullPath=$1
 author=${fullPath##*/}	# https://stackoverflow.com/a/1371283
 
-ls $fullPath | parallel -S node2 -S node5 -S node6 -S node7 -S node8 -S node9 -S node10 -S node11 -S node12 -S node14 -S node15 -S node16 -k --bar prime-commits-extract -d ${fullPath}/{} -l ../../data/commits/${author}_{}_log.log -o ../../data/commits/${author}_{}_commits_loc.json
+mkdir $dataDir
+
+
+ls $fullPath | parallel -S node2 -S node5 -S node6 -S node7 -S node8 -S node9 -S node10 -S node11 -S node12 -S node14 -S node15 -S node16 -k --bar prime-commits-extract -d ${fullPath}/{} -l ${dataDir}/${author}_{}_log.log -o ${dataDir}/${author}_{}_commits_loc.json
