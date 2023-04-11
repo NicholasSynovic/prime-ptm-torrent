16d5f287ddb8a4ad032ca2b06cdda9d514fc62da
Nicholas Synovic <nicholas.synovic@gmail.com>
2023-04-11 14:19:42 -0500

    Update commitsPipeline.bash

--- prime_ptm_torrent/commitsPipeline.bash
+++ prime_ptm_torrent/commitsPipeline.bash
@@ -7,4 +7,4 @@ author=${fullPath##*/}	# https://stackoverflow.com/a/1371283
 mkdir -p $dataDir
 
 
-ls $fullPath | sshpass parallel --sshdelay 0.1 -S node2 -S node5 -S node6 -S node7 -S node8 -S node9 -S node10 -S node11 -S node12 -S node14 -S node15 -S node16 -k --bar prime-commits-extract -d ${fullPath}/{} -l ${dataDir}/${author}_{}_log.log -o ${dataDir}/${author}_{}_commits_loc.json
+ls $fullPath | parallel -k --bar prime-commits-extract -d ${fullPath}/{} -l /home/nsynovic/data/commits/${author}_{}_log.log -o /home/nsynovic/data/commits/${author}_{}_commits_loc.json
