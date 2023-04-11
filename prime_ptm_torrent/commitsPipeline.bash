39ef0c27006c6e7c0f42afc51636f8b42e04d3b1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-11 12:52:54 -0500

    Add additional parameters

--- prime_ptm_torrent/commitsPipeline.bash
+++ prime_ptm_torrent/commitsPipeline.bash
@@ -7,4 +7,4 @@ author=${fullPath##*/}	# https://stackoverflow.com/a/1371283
 mkdir -p $dataDir
 
 
-ls $fullPath | parallel -S node2 -S node5 -S node6 -S node7 -S node8 -S node9 -S node10 -S node11 -S node12 -S node14 -S node15 -S node16 -k --bar prime-commits-extract -d ${fullPath}/{} -l ${dataDir}/${author}_{}_log.log -o ${dataDir}/${author}_{}_commits_loc.json
+ls $fullPath | sshpass parallel --sshdelay 0.1 -S node2 -S node5 -S node6 -S node7 -S node8 -S node9 -S node10 -S node11 -S node12 -S node14 -S node15 -S node16 -k --bar prime-commits-extract -d ${fullPath}/{} -l ${dataDir}/${author}_{}_log.log -o ${dataDir}/${author}_{}_commits_loc.json
