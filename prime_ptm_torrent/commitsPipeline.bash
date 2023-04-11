d74c00dd86730fb2a6e9d19d3e3961fff9110188
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-11 12:51:31 -0500

    Create parent directories

--- prime_ptm_torrent/commitsPipeline.bash
+++ prime_ptm_torrent/commitsPipeline.bash
@@ -4,7 +4,7 @@ dataDir="~/data/commits"
 fullPath=$1
 author=${fullPath##*/}	# https://stackoverflow.com/a/1371283
 
-mkdir $dataDir
+mkdir -p $dataDir
 
 
 ls $fullPath | parallel -S node2 -S node5 -S node6 -S node7 -S node8 -S node9 -S node10 -S node11 -S node12 -S node14 -S node15 -S node16 -k --bar prime-commits-extract -d ${fullPath}/{} -l ${dataDir}/${author}_{}_log.log -o ${dataDir}/${author}_{}_commits_loc.json
