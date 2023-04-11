0dfb722a70cb6e79e6b5ef1da4c74abd62ee2116
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-11 12:39:46 -0500

    Update commits pipeline

--- prime_ptm_torrent/commitsPipeline.bash
+++ prime_ptm_torrent/commitsPipeline.bash
@@ -5,4 +5,4 @@ mkdir ../../data/commits
 fullPath=$1
 author=${fullPath##*/}	# https://stackoverflow.com/a/1371283
 
-ls $fullPath | parallel -j 10 -k --bar clime-git-commits-extract -d ${fullPath}/{} -o ../../data/commits/${author}_{}_commits_loc.json --log ../../data/commits/${author}_{}_log.log
+ls $fullPath | parallel -S node2 -S node5 -S node6 -S node7 -S node8 -S node9 -S node10 -S node11 -S node12 -S node14 -S node15 -S node16 -k --bar prime-commits-extract -d ${fullPath}/{} -l ../../data/commits/${author}_{}_log.log -o ../../data/commits/${author}_{}_commits_loc.json
